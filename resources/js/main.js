d980f4c213277076d303a071949b4fb2caa4ba0e
andreloezer <andrecesarloezer@gmail.com>
2017-06-25 01:40:24 +0000

    Carousel

---
+++ resources/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $('.carousel').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: false,
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+  });
+})
